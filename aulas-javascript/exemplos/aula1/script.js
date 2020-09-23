cea99114277ce4c1b5bd35957853544b9462ffa1
Rita Lino <rita.rlino@gmail.com>
2020-09-22 21:05:52 -0300

    aula 1 - javascript

---
+++ aulas-javascript/exemplos/aula1/script.js
@@ -0,0 +1,45 @@
+//console.log('Olá meninas!');
+
+//alert('Olá mundo!');
+
+/*
+let nome = 'Rita';
+console.log(nome);
+
+nome = 'Laryssa';
+console.log(nome);
+*/
+
+//let nome = 'Rita';
+
+//console.log(`Meu nome é ${nome}`);
+
+//let nome = prompt('Qual é seu nome?');
+
+//console.log(nome);
+
+/*function mostrarNome(nome, sobrenome) {
+  console.log(`${nome} ${sobrenome}`);
+}
+
+mostrarNome('Rita', 'Lino');
+
+function somarNumeros(numero1, numero2) {
+  console.log(numero1 + numero2);
+}
+
+somarNumeros(20, 2);
+*/
+
+/*function checarNumero() {
+  let numero = prompt('Digite um número');
+
+  if (numero > 20) {
+    alert('É maior que 20!');
+  } else {
+    alert('Não é maior que 20!');
+  }
+}
+
+checarNumero();
+*/
